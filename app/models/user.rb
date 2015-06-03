990c3d8ca25d42ccea4a5d22bd13c87523661a17
Roger <roger@Rogers-iMac.local>
2015-06-02 21:57:10 -0500

    Login now allows user to use username or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,10 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+    :recoverable, :rememberable, :trackable, :validatable
   validates :email, uniqueness: true
   validates :username, :email, :password, :firstname, :lastname, :country, presence: true
-
+  #validates :username, :uniqueness: {:case_sensitive => false} 
   after_update :send_promote_notification, :if => proc{points_changed? && points >=1400 && points_was <1400 && !trailblazer}
 
   scope :trailblazer, -> {where(trailblazer: true)}
@@ -20,17 +20,24 @@ class User < ActiveRecord::Base
   scope :trekker, -> {where(trailblazer: false)}
   scope :topusers, -> {order(points: :desc).limit(10)}
 
+
+  def send_promote_notification
+    note = Notification.new(message: "You have been invited to become a Trailblazer.", link: "<a data-method=\"patch\" href=\"\/PromoteTrailblazer\" id=\"#{id}\">Accept<\/a>", user_id: id)
+    note.save
+  end
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.username || self.email
+  end
   def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
-      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
     else
-      where(conditions).first
+      where(conditions.to_hash).first
     end
-  end
-
-  def send_promote_notification
-      note = Notification.new(message: "You have been invited to become a Trailblazer.", link: "<a data-method=\"patch\" href=\"\/PromoteTrailblazer\" id=\"#{id}\">Accept<\/a>", user_id: id)
-      note.save
   end
 end
